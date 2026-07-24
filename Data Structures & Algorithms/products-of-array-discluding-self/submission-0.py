47f185afdd399b6d0d827fed038eb6e189835109
soloLevling-hacker <khushrupapara5@gmail.com>
2026-07-24 14:25:55 +0530

    Change result array to mal in productExceptSelf
    Refactor productExceptSelf method to use 'mal' array instead of 'res'.

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -77,3 +77,24 @@ class Solution:
             res[i] *= right
             right *= nums[i]
         return res
+
+
+"""
+or you can use this also
+"""
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        mal = [1] * n
+        left = 1
+        right = 1
+
+        for i in range(n):
+            mal[i] *= left
+            left *= nums[i]
+
+        for i in range(n-1, -1, -1):
+            mal[i] *= right
+            right *= nums[i]
+
+        return mal
